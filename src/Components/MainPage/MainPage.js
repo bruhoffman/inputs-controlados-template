2233081312984ef3703ef7b89a0e89e1ebd9da13
bruhoffman <bruhoffman@gmail.com>
2024-02-29 17:29:15 -0300

    Exercícios da Aula

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,23 +1,45 @@
-import React from 'react'
-import { MainContainer, Form, Input } from './styles'
+import React, { useState } from "react";
+import { MainContainer, Form, Input, Select } from "./styles";
 
 function MainPage() {
+  const [nome, setNome] = useState("");
+  const nomeChange = (event) => {
+    setNome(event.target.value);
+  };
+
+  const [idade, setIdade] = useState("");
+  const idadeChange = (event) => {
+    setIdade(event.target.value);
+  };
+
+  const [estadoCivil, setEstadoCivil] = useState("");
+  const mudarEstadoCivil = (event) => {
+    setEstadoCivil(event.target.value);
+  };
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       <Form>
         <label>
           Nome:
-          <Input />
+          <Input value={nome} onChange={nomeChange} />
         </label>
         <label>
           Idade:
-          <Input />
+          <Input value={idade} onChange={idadeChange} />
         </label>
+        <Select value={estadoCivil} onChange={mudarEstadoCivil}>
+          <option>Nenhum</option>
+          <option>Solteiro(a)</option>
+          <option>Casado(a)</option>
+          <option>Viuvo(a)</option>
+          <option>Divorciado(a)</option>
+        </Select>
         <button>Enviar dados</button>
       </Form>
     </MainContainer>
-  )
+  );
 }
 
-export default MainPage
+export default MainPage;
