04b128a462cdff291bf5921698e455635fc63bbb
BrunoHoffman <bruhoffman@gmail.com>
2024-03-04 22:09:35 -0300

    Resolução dos exercícios da aula

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,20 +1,83 @@
-import React from 'react'
-import { MainContainer, Form, Input } from './styles'
+import React, { useState } from 'react'
+import { MainContainer, Form, Input, Selection, Botao } from './styles'
 
 function MainPage() {
+  const [nome, setNome] = useState("");
+  const [idade, setIdade] = useState("");
+  const [estadoCivil, setEstadoCivil] = useState();
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const onChangeNome = (event) => {
+    setNome(event.target.value);
+  };
+
+  const onChangeIdade = (event) => {
+    setIdade(event.target.value);
+  };
+
+  const onChangeEstadoCivil = (event) => {
+    setEstadoCivil(event.target.value);
+  };
+
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onChangeSenha = (event) => {
+    setSenha(event.target.value);
+  };
+
+  function imprimirValoresDosInputs(event) {
+    event.preventDefault();
+    console.log(`Nome: ${nome}`);
+    console.log(`Idade: ${idade}`);
+    console.log(`Estado Civil: ${estadoCivil}`);
+    console.log(`E-mail: ${email}`);
+    console.log(`Senha: ${senha}`);
+
+    limpartInputs();
+  }
+
+  function limpartInputs() {
+    setNome(" ");
+    setIdade(" ");
+    setEstadoCivil(" ");
+    setEmail(" ");
+    setSenha(" ");
+  }
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
-      <Form>
+      <Form onSubmit={imprimirValoresDosInputs}>
         <label>
           Nome:
-          <Input />
+          <Input onChange={onChangeNome} />
         </label>
         <label>
           Idade:
-          <Input />
+          <Input onChange={onChangeIdade} />
         </label>
-        <button>Enviar dados</button>
+        <label>
+          Estado Civil:
+          <Selection onChange={onChangeEstadoCivil}>
+            <option></option>
+            <option>Solteiro(a)</option>
+            <option>Casado(a)</option>
+            <option>Divorciado(a)</option>
+            <option>Viúvo(a)</option>
+          </Selection>
+        </label>
+        <label>
+          E-mail:
+          <Input type='email' onChange={onChangeEmail} />
+        </label>
+        <label>
+          Senha:
+          <Input type='password' onChange={onChangeSenha} />
+        </label>
+        <Botao>Enviar dados</Botao>
       </Form>
     </MainContainer>
   )
